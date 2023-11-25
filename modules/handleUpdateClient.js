3fe8767f620d6a9a668b29fd59c78de5889260ac
ruuuu <rufinka_91@mail.ru>
2023-11-25 18:43:44 +0300

    Add files via upload

--- modules/handleUpdateClient.js
+++ modules/handleUpdateClient.js
@@ -9,20 +9,6 @@ import fs from "node:fs/promises";
 
 export const handleUpdateClient = async(req, res, ticketNumber) => {
 
-      try{
-            const clientsData = await fs.readFile(CLIENTS, "utf-8");                // запишем в clientsData содержимое файла CLIENTS
-            const clients = JSON.parse(clientsData);                                    // [{fullName, phone, ticketNumber},{},{}]
-            let ind = 0;
-
-
-            const client = clients.find((item, index) => {
-                  ind = index;
-                  return (item.ticketNumber === ticketNumber);
-            });
-           
-            console.log('client object for /clients/:ticket',  client);
-
-
             let body = "";                                                    // тело запроса, котрый отправляет клиент
 
             try{
@@ -38,24 +24,51 @@ export const handleUpdateClient = async(req, res, ticketNumber) => {
 
 
             req.on("end", async () => {   // при откравке запроса PACTH  отрабоатет коллбэк
-                  console.log('body ', body);                      // это то, что клиент отправляет: {"fullName":"Ru Ivanova",  "phone":"89765456",  "ticketNumber":"895"}
+                  
+                  //console.log('body ', body);                      // это то, что клиент отправляет: {"fullName":"Ru Ivanova",  "phone":"89765456",  "ticketNumber":"895"}
                   const updateClient = JSON.parse(body);                // {fullName, phone, ticketNumber, booking: [{}]}
-                  console.log('updateClient ', updateClient)
-            
-                  clients.splice(ind, 1, updateClient);                 // удалили элеент с индексом ind  из массива и вставили элемент updateClient
+                 // console.log('updateClient ', updateClient)
+                 
+                  if(!updateClient.fullName || !updateClient.phone ||  ! updateClient.ticketNumber || !updateClient.booking){
+                        sendError(res, 400, 'Неверные основные данные');
+                        return;
+                  }
+
+
+      
+                  if(updateClient.booking && (!updateClient.booking.length || !Array.isArray(updateClient.booking) || !updateClient.booking.every((item) => item.comedian && item.time))){
+                        sendError(res, 400, 'отсутсвует список бронирования');
+                        return;
+                  }
+
+
+                  const clientsData = await fs.readFile(CLIENTS, "utf-8");                // запишем в clientsData содержимое файла CLIENTS
+                  const clients = JSON.parse(clientsData);                                    // [{fullName, phone, ticketNumber},{},{}]
+                  
+                  const clientIndex = clients.findIndex((item) => {                             // вернет индекс того элмнта котрый удовлетворяет условию
+                        return (item.ticketNumber === ticketNumber);
+                  });
+                 
+                  console.log('client object for /clients/:ticket',  clientIndex);
+      
+                  if(clientIndex === -1){ // если такого элемнта нет
+                        sendError(res, 400, 'клиент с данными не существует');
+                  }
+
+                  clients.splice(clientIndex, 1, updateClient);                 // удалили элеент с индексом clientIndex  из массива и вставили элемент updateClient
 
                   await fs.writeFile(CLIENTS, JSON.stringify(clients));                   // в файле будет [ {"fullName":"Ru Ivanova", "phone":"89765456", "ticketNumber":"895"} ]
       
                   sendData(res, updateClient);            // сервер посылает ответ клиенту
       
             });  
-      }
-      catch(error){
-            console.error(`Ошибка при обработке запроса ${error}`);
-            sendError(res, 500, 'Ошибпри при обработке запроска клиента');
-      }
 
+           
 
-
-
+            
 };
+      
+
+
+
+
